80bf0b4ad54e009d4b211d98b199cf750ddb7990
akitsuhoshiyomi <154935992+akitsuhoshiyomi@users.noreply.github.com>
2024-03-31 18:19:00 -0400

    Also added basic tabs and a title
    Labels and tabs are added for flavor in our setup.

---
+++ warrior_chat/lib/pages/home.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import "page1.dart";
+import "page2.dart";
+
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context){
+    return const Material(
+
+      child : MainTabBar()
+      
+    );
+  }
+}
+
+class MainTabBar extends StatelessWidget{
+  const MainTabBar({super.key});
+
+  @override 
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home : DefaultTabController(
+        length : 2,
+        child: Scaffold(
+          appBar: AppBar(
+            
+            title : const Text("Warrior Chat"),
+            backgroundColor: Colors.blue,
+            bottom: TabBar(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+
+              indicatorSize: TabBarIndicatorSize.tab,
+              unselectedLabelColor: Colors.white,
+              
+              
+              labelColor: Colors.blue,
+              indicator: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10))
+              ),
+              tabs: [
+                Tab(
+                  child : Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.transparent
+                    ),
+                    child : const Align(
+                      child: Text("Overview"),
+                    )
+                  )
+                ),
+                Tab(
+                  child : Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5)
+                  ),
+                  child : const Align(
+                    child: Text("People"),
+                  )
+                  )
+                ),
+              ])
+          ),
+          body: const TabBarView(
+            children: [
+              Page1(),
+              Page2()
+            ]
+          )
+        )
+      )
+    );
+  }
+}
